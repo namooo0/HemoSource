604d91ccf4871db359e1e0041433692c3620914f
Noura Moussa <nmous003@cslab-163.cs.plattsburgh.edu>
2018-09-05 15:49:34 -0400

    Started script to download names to a database

---
+++ hosName.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#This is a script that will take csv and take out hospitals names to put
+#into the database
+
+from glob import *
+from os import *
+
+#import database or link it somehow
+#need to find then import csv
+
+#parse through csv for hospital names
+
+#put names in database
