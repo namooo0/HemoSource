6d2d22c8f6348b699fb48fd7932629666847aa66
Noura Moussa <nmous003@cslab-114.cs.plattsburgh.edu>
2018-10-01 19:47:11 -0400

    found problem

--- hosName.py
+++ hosName.py
@@ -10,19 +10,27 @@ cv_f = input_file
 '''hosName finally gets all hospital names
    need to get only one copy of each'''
 
-
+'''
+    program is goin through and adding albany, next hospital, alb, next hos
+    other hos, alb
+    ugh
+'''
 hosNames = []
 for row in cv_f:
     getShit = row["Hospital Name"]
+    #print(getShit)
     hosNames.append(getShit)
     #print(getShit)
     #print(hosNames) #WORKS!!!!
     seen = set()
     result = []
     for item in hosNames:
+        
         if item not in seen:
             seen.add(item)
-            result.append(seen)
+            result.append(item)
+
+print(len(result))     
 
 with open('listOfNames.txt', 'w') as f:
 	for item in result:
